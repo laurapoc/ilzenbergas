20b067f20dca81d659b09cb2e2217ed5e277f5a3
laurapoc <laura.pociuniene@gmail.com>
2020-04-05 23:39:35 +0300

    WIP farm_header and water page

---
+++ assets/scripts/farm_header.js
@@ -0,0 +1,65 @@
+
+// SHOW NAVBAR DROPDOWN'S MENU
+$("div.dropdown li.dropdown").hover(
+    function() {
+      $(this)
+        .find(".dropdown-menu")
+        .stop(true, true)
+        .delay(200)
+        .fadeIn(500);
+    },
+    function() {
+      $(this)
+        .find(".dropdown-menu")
+        .stop(true, true)
+        .delay(200)
+        .fadeOut(500);
+    }
+  );
+  
+  // CHANGE ICONS COLOR ON HOVER
+  let img = document.querySelectorAll(".nav-link img");
+  img.forEach(element => {
+    changeIconColor(element);
+  });
+  
+  // CHANGE ICON'S COLOR ON HOVER
+  
+  function changeIconColor(htmlElement) {
+    htmlElement.addEventListener("mousemove", function(event) {
+      event.target.src = event.target.src.replace("_grey", "_yelow");
+    });
+    htmlElement.addEventListener("mouseleave", function(event) {
+      event.target.src = event.target.src.replace("_yelow", "_grey");
+    });
+  }
+  let mapIcon = document.querySelector("#map-icon img");
+  changeIconColor(mapIcon);
+  
+  // changing header icons color
+  let url = pageName;
+  if (url.includes("water")) {
+    document.querySelector("#water").src = "assets/img/round_icons/vanduo_yelow.png";
+  } else if (url.includes("map")) {
+    document.querySelector("#map").src = "assets/img/round_icons/map_yelow.png";
+  } else if (url.includes("events")) {
+    document.querySelector("#events").src = "assets/img/round_icons/renginiai_yelow.png";
+  } else if (url.includes("excursions")) {
+    document.querySelector("#excursions").src = "assets/img/round_icons/ekskursijos_yelow.png";
+  } else if (url.includes("tastings")) {
+    document.querySelector("#tastings").src = "assets/img/round_icons/degustacijos_yelow.png";
+  } else if (url.includes("restaurant")) {
+    document.querySelector("#restaurant").src = "assets/img/round_icons/restoranas_yelow.png";
+  } else if (url.includes("accommodation")) {
+    document.querySelector("#accommodation").src = "assets/img/round_icons/nakvyne_yelow.png";
+    console.log("accommodation cklicked");
+  } else if (url.includes("potato")) {
+    document.querySelector("#potato").src = "assets/img/round_icons/potato_yelow.png";
+    console.log("icon clicked");
+  };
+   
+  // hiding menu items after clicking
+  $(".nav-link").on("click", function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+  
