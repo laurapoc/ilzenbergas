e82eca5066bed6cae5ddb8c26eabc21d88847f56
laurapoc <laura.pociuniene@gmail.com>
2020-04-08 22:37:34 +0300

    farm and food pages created

---
+++ assets/scripts/farm.js
@@ -0,0 +1,38 @@
+let pageName = "farm";
+
+let templateToLoad = "basis_templ1";
+let selectedMenuItem = "a";
+let sideMenuItems = [];
+let subTemplateData = {};
+// let sidebarData = {};
+let gallerySource = [];
+
+// IMPORTING TEMPLATES:
+fetch("./assets/json/farm_data.json")
+  .then(response => response.json())
+  .then(sidebarData => {
+    console.log("farm data");
+    // clone template to create menu item
+    sideMenuItems = sidebarData.sideMenuItems;
+    sidebarData =  sidebarData.sideMenuItems;
+    // IMPORTING MAIN MENU
+    importTemplate("./farm_header.html", "#farm_header", "./assets/scripts/farm_header.js");
+
+    // // IMPORTING LEFT SIDE MENU
+    importTemplate("./sidebar.html", "#sidebar", "./assets/scripts/sidebar.js");
+
+    // IMPORTING ONE OF PARK TEMPLATES
+    subTemplateData = sideMenuItems[0];
+    importTemplate("./" + templateToLoad + ".html", "#" + templateToLoad, "./assets/scripts/" + templateToLoad + ".js");
+    //loadBasisTempl1(sidebarData);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+
+// IMPORTING FOOTER
+importTemplate("./footer.html", "#footer", "./assets/scripts/footer.js");
+
+// IMPORTING BACKGROUND
+importTemplate("./background.html", "#background", null);
+
---
+++ assets/scripts/farm_map.js
@@ -0,0 +1,8 @@
+let pageName = "farm_map";
+console.log(pageName);
+
+// IMPORTING MAIN MENU
+importTemplate("./farm_header.html", "#farm_header", "./assets/scripts/farm_header.js");
+
+// IMPORTING BACKGROUND
+importTemplate("./background.html", "#background", null);
