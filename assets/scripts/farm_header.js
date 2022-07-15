4160cb233d77c60d5fa3a18ad4549bbfc54ef33d
Laura Pociuniene <laurapociuniene@gmail.com>
2022-07-15 08:50:20 +0300

    remove agriculture icon from farm_header

--- assets/scripts/farm_header.js
+++ assets/scripts/farm_header.js
@@ -1,5 +1,9 @@
 /*jshint esversion: 6 */
-import { changeIconColor, changeLangValue, loadCookieBaner } from "./functions.js";
+import {
+  changeIconColor,
+  changeLangValue,
+  loadCookieBaner,
+} from "./functions.js";
 
 // changing header icons color
 export function setupHeader(pageName) {
@@ -30,28 +34,32 @@ export function setupHeader(pageName) {
   pageName = pageName;
   switch (pageName) {
     case "water":
-      document.querySelector("#water").src = "assets/img/round_icons/vanduo_yelow.png";
+      document.querySelector("#water").src =
+        "assets/img/round_icons/vanduo_yelow.png";
       break;
     case "farm":
-      document.querySelector("#open-farm").src = "assets/img/round_icons/ukis_yelow.png";
+      document.querySelector("#open-farm").src =
+        "assets/img/round_icons/ukis_yelow.png";
       break;
     case "food":
-      document.querySelector("#natural-food").src = "assets/img/round_icons/produktai_yelow.png";
+      document.querySelector("#natural-food").src =
+        "assets/img/round_icons/produktai_yelow.png";
       break;
     case "wine":
-      document.querySelector("#wine").src = "assets/img/round_icons/vynas_yelow.png";
+      document.querySelector("#wine").src =
+        "assets/img/round_icons/vynas_yelow.png";
       break;
     case "shoping areas":
-      document.querySelector("#shop-areas").src = "assets/img/round_icons/krautuves_yelow.png";
-      break;
-    case "agriculture":
-      document.querySelector("#agriculture").src = "assets/img/round_icons/ukininkyste_yelow.png";
+      document.querySelector("#shop-areas").src =
+        "assets/img/round_icons/krautuves_yelow.png";
       break;
     case "principles":
-      document.querySelector("#principles").src = "assets/img/round_icons/principai1_yelow.png";
+      document.querySelector("#principles").src =
+        "assets/img/round_icons/principai1_yelow.png";
       break;
     case "farm_map":
-      document.querySelector("#map").src = "assets/img/round_icons/map_yelow.png";
+      document.querySelector("#map").src =
+        "assets/img/round_icons/map_yelow.png";
       break;
   }
 
